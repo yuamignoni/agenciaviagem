9cd5a87a9b66e2f9978d87808fa69632d513890f
Yuarã Mignoni <yuara.mignoni@ozmap.com.br>
2024-11-26 18:06:33 -0300

    project created

---
+++ src/main/java/com/desafio/agenciaviagem/AgenciaviagemApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.agenciaviagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaviagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaviagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/agenciaviagem/AgenciaviagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.agenciaviagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaviagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
